4043a0e8a55a6104bcf22b69b103d6bcb3f83afa
michalcecko <ceckomichal@gmail.com>
2025-07-26 15:48:54 +0200

    Add `ASSET_URL` environment variable in `docker-compose.prod.yml`.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(append: [
             ApiMiddleware::class,
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
