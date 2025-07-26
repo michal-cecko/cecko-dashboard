804b981fc19bd4386723837fcfbfccdaff1cd78d
michalcecko <ceckomichal@gmail.com>
2025-07-26 14:13:34 +0200

    Update `docker-compose.prod.yml` to add timeout middleware for `synapps-dashboard` and enhance service configuration; add test route in `web.php`.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect(route("filament.songs.home"));
 });
+
+// Simple working route for now
+Route::get('/test', function () {
+    echo "OK!";
+});
