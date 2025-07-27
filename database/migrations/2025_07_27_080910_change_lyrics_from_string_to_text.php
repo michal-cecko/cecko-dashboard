8c52c2d4a94c698b439a70d6976bc0e791e3b14e
michalcecko <ceckomichal@gmail.com>
2025-07-27 10:11:21 +0200

    Add `ASSET_URL` environment variable in `docker-compose.prod.yml`.

--- database/migrations/2025_07_27_080910_change_lyrics_from_string_to_text.php
+++ database/migrations/2025_07_27_080910_change_lyrics_from_string_to_text.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('text', function (Blueprint $table) {
+        Schema::table('songs', function (Blueprint $table) {
             $table->text('lyrics')->change();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('text', function (Blueprint $table) {
+        Schema::table('songs', function (Blueprint $table) {
             $table->text('lyrics')->change();
         });
     }
