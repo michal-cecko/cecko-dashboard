26f9ac91ec53a0f39574e000cacd7900faf58724
michalcecko <ceckomichal@gmail.com>
2026-02-23 09:45:42 +0100

    Update email logo rendering: add `object-fit: contain` and auto dimensions for improved responsiveness

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if($logoUrl)
-<img src="{{ $logoUrl }}" class="logo" alt="{{ $slot }}" style="max-width: 200px; max-height: 80px;">
+<img src="{{ $logoUrl }}" class="logo" alt="{{ $slot }}" style="max-width: 200px; max-height: 80px; width: auto; height: auto; object-fit: contain;">
 @else
 {!! $slot !!}
 @endif
