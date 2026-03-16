3167ddd09bb203be994a458f379ff2313f79e8b5
michalcecko <ceckomichal@gmail.com>
2026-03-16 19:43:08 +0100

    Add PayBySquareService tests and adjust due date handling logic in QR code generation.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\ApiMiddleware;
+use App\Http\Middleware\NoIndexHeaders;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
             ApiMiddleware::class,
         ]);
         $middleware->trustProxies(at: '*');
+        $middleware->web(append: [
+            NoIndexHeaders::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
