7454df8cdb8642fcc10ba4d6714e6cdb282a1ba2
michalcecko <ceckomichal@gmail.com>
2026-03-28 12:32:53 +0100

    Update `InvoiceEmailLog` to use `Common\User` model instead of `User`.

--- app/Models/Invoices/InvoiceEmailLog.php
+++ app/Models/Invoices/InvoiceEmailLog.php
@@ -2,7 +2,7 @@
 
 namespace App\Models\Invoices;
 
-use App\Models\User;
+use App\Models\Common\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
