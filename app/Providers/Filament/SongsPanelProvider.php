7e96752c16a888dadfb0e0588d80ce325562ab8b
michalcecko <ceckomichal@gmail.com>
2026-03-28 06:15:14 +0100

    Upgrade Laravel framework to 13.x, update related dependencies, and replace `VerifyCsrfToken` with `PreventRequestForgery`.

--- app/Providers/Filament/SongsPanelProvider.php
+++ app/Providers/Filament/SongsPanelProvider.php
@@ -20,7 +20,7 @@ use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
-use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Http\Middleware\PreventRequestForgery;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
@@ -67,7 +67,7 @@ class SongsPanelProvider extends PanelProvider
                 StartSession::class,
                 AuthenticateSession::class,
                 ShareErrorsFromSession::class,
-                VerifyCsrfToken::class,
+                PreventRequestForgery::class,
                 SubstituteBindings::class,
                 DisableBladeIconComponents::class,
                 DispatchServingFilamentEvent::class,
