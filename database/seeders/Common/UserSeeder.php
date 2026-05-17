41a89ed4a827fc837702ffe751c510bd51e7cf95
michalcecko <ceckomichal@gmail.com>
2026-05-17 12:57:00 +0200

    Inject COMPOSER_AUTH at build time; move seeder password to env var

--- database/seeders/Common/UserSeeder.php
+++ database/seeders/Common/UserSeeder.php
@@ -10,24 +10,26 @@ class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        $password = bcrypt(env('SEED_USER_PASSWORD', bin2hex(random_bytes(16))));
+
         User::factory()->create([
             'name' => 'Tatino',
             'email' => 'hippo.vd@gmail.com',
-            'password' => bcrypt('***REMOVED***'),
+            'password' => $password,
             'capabilities' => [UserCapabilityEnum::VIEW_SONGS, UserCapabilityEnum::MANAGE_SONGS, UserCapabilityEnum::VIEW_MOBILE_APPS],
         ]);
 
         User::factory()->create([
             'name' => 'Emik',
             'email' => 'emca.sk2918@gmail.com',
-            'password' => bcrypt('***REMOVED***'),
+            'password' => $password,
             'capabilities' => [UserCapabilityEnum::VIEW_SONGS, UserCapabilityEnum::MANAGE_SONGS, UserCapabilityEnum::VIEW_MOBILE_APPS],
         ]);
 
         User::factory()->create([
             'name' => 'Mišus',
             'email' => 'michal.cecko@gmail.com',
-            'password' => bcrypt('***REMOVED***'),
+            'password' => $password,
             'capabilities' => UserCapabilityEnum::cases(),
         ]);
     }
